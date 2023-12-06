d322722583a7865bcf27ad14db6a3fc65cecfcf9
Aayush Gupta <aayushnj@gmail.com>
2023-12-06 00:04:59 +0000

    added gitignore

--- build/pc_merger/CMakeFiles/pc_merger_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pc_merger_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pc_merger_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/pc_merger/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/bags/pc_merger_ws/src/pc_meger
-# Build directory: /home/ubuntu/bags/pc_merger_ws/src/build/pc_merger
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
